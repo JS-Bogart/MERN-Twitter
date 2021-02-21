3c384d1f939a7c494f85e15507f9ca0bc2301fe7
Bogey <jeffreysbogart@gmail.com>
2021-02-21 15:53:20 -0800

    Add tweets reducer and container

--- frontend/src/reducers/root_reducer.js
+++ frontend/src/reducers/root_reducer.js
@@ -1,8 +1,12 @@
 import { combineReducers } from 'redux';
 import session from './session_reducer';
+import TweetsReducer from './tweets_reducer';
+import errors from './errors_reducer';
 
 const RootReducer = combineReducers({
-  session
+  session,
+  errors,
+  tweets: TweetsReducer
 });
 
 export default RootReducer;
